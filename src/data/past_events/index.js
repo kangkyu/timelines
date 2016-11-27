8f9bc56ea135b605bbaf101a10dd1e594a15db87
server-monitor <server.monitor.lizard@gmail.com>
2016-11-27 04:58:29 -0800

    ...

--- src/data/past_events/index.js
+++ src/data/past_events/index.js
@@ -1,91 +1,91 @@
 
-import 'isomorphic-fetch';
+// import 'isomorphic-fetch';
 
-import getNextLink from './next_link';
-import { proxiedURL, reqInit } from './request';
-import signedURLs from './signed_urls.json';
+// import getNextLink from './next_link';
+// import { proxiedURL, reqInit } from './request';
+// import signedURLs from './signed_urls.json';
 
-import * as Util from '../../util/index';
+// import * as Util from '../../util/index';
 
-const getData = (json) => {
-  const upcomingEventFound = json.find(ev => ev.status === 'upcoming');
+// const getData = (json) => {
+//   const upcomingEventFound = json.find(ev => ev.status === 'upcoming');
 
-  let data;
-  if (upcomingEventFound) {
-    data = json.filter(ev => ev.status === 'past');
-  } else {
-    data = json;
-  }
+//   let data;
+//   if (upcomingEventFound) {
+//     data = json.filter(ev => ev.status === 'past');
+//   } else {
+//     data = json;
+//   }
 
-  return {
-    payload: data,
-    upcomingEventFound
-  };
-};
+//   return {
+//     payload: data,
+//     upcomingEventFound
+//   };
+// };
 
-const ok = (response) => {
-  if (!response.ok) {
-    const { status, statusText, url } = response;
-    console.error(
-      'Response not OK\n',
-      `Status: ${status}, text: '${statusText}', URL: ...\n`,
-      url
-    );
-    return false;
-  }
-  return response;
-};
+// const ok = (response) => {
+//   if (!response.ok) {
+//     const { status, statusText, url } = response;
+//     console.error(
+//       'Response not OK\n',
+//       `Status: ${status}, text: '${statusText}', URL: ...\n`,
+//       url
+//     );
+//     return false;
+//   }
+//   return response;
+// };
 
-const buildURL = (fullURL, apiKey) => {
-  const proxify = proxiedURL(fullURL);
-  // if (!apiKey) return proxify;
+// const buildURL = (fullURL, apiKey) => {
+//   const proxify = proxiedURL(fullURL);
+//   // if (!apiKey) return proxify;
 
-  const urlWithAccessToken = `${proxify}/events?key=${apiKey}&photo-host=public&sign=true&page=200&status=past&omit=description,how_to_find_us`;
+//   const urlWithAccessToken = `${proxify}/events?key=${apiKey}&photo-host=public&sign=true&page=200&status=past&omit=description,how_to_find_us`;
 
-  return new Request(urlWithAccessToken, reqInit);
-  // return [urlWithAccessToken, requestArgs[1]];
-};
+//   return new Request(urlWithAccessToken, reqInit);
+//   // return [urlWithAccessToken, requestArgs[1]];
+// };
 
-const fetchPastEvents = (fullURL, groupEvents, apiKey) => {
-  const request = buildURL(fullURL, apiKey);
+// const fetchPastEvents = (fullURL, groupEvents, apiKey) => {
+//   const request = buildURL(fullURL, apiKey);
 
-  return fetch(request)
-    .then(response => (ok(response) ? response : Promise.reject('...')))
-    .then((response) => {
-      const headers = response.headers;
+//   return fetch(request)
+//     .then(response => (ok(response) ? response : Promise.reject('...')))
+//     .then((response) => {
+//       const headers = response.headers;
 
-      return response.json().then((json) => {
-        const data = getData(json);
+//       return response.json().then((json) => {
+//         const data = getData(json);
 
-        groupEvents.push(...data.payload);
+//         groupEvents.push(...data.payload);
 
-        const returnObj = {
-          pastEvents: groupEvents
-        };
+//         const returnObj = {
+//           pastEvents: groupEvents
+//         };
 
-        if (data.upcomingEventFound) return returnObj;
+//         if (data.upcomingEventFound) return returnObj;
 
-        const nextLink = getNextLink(headers);
-        if (!nextLink) return returnObj;
+//         const nextLink = getNextLink(headers);
+//         if (!nextLink) return returnObj;
 
-        return fetchPastEvents(nextLink, groupEvents);
-      });
-    })
-    .catch(err => console.error('fetch operation problem...', err));
-};
+//         return fetchPastEvents(nextLink, groupEvents);
+//       });
+//     })
+//     .catch(err => console.error('fetch operation problem...', err));
+// };
 
-export default (urlArg = null, apiKey = null) => {
-  let urls;
-  if (urlArg) {
-    Util.assertNotBlank('apiKey', apiKey);
-    urls = [urlArg];
-  } else {
-    urls = signedURLs.map(({ pastEvents }) => pastEvents);
-  }
+// export default (urlArg = null, apiKey = null) => {
+//   let urls;
+//   if (urlArg) {
+//     Util.assertNotBlank('apiKey', apiKey);
+//     urls = [urlArg];
+//   } else {
+//     urls = signedURLs.map(({ pastEvents }) => pastEvents);
+//   }
 
-  return urls.map((url) => {
-    const groupEvents = [];
+//   return urls.map((url) => {
+//     const groupEvents = [];
 
-    return fetchPastEvents(url, groupEvents, apiKey);
-  });
-};
+//     return fetchPastEvents(url, groupEvents, apiKey);
+//   });
+// };
